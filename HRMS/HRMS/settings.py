f84ed03a2c7f43682ff84e9b5c82c478706d39c2
skumbharkar <sachin112029@gmail.com>
2020-01-06 01:36:22 -0600

    send mail functionality added

--- HRMS/HRMS/settings.py
+++ HRMS/HRMS/settings.py
@@ -27,10 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-EMAIL_HOST = 'abinitio.exusia.com'
-EMAIL_PORT = 25
+
+EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "sachin112029@gmail.com"
+EMAIL_HOST_PASSWORD = '9527415992'
 
 # Application definition
 
